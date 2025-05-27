af0d7456306f1468241f908872a9351370cf5e5e
Afnan <talktoafnanali@gmail.com>
2025-05-27 21:04:42 +0500

    Initial commit

--- middleware/verify-token.js
+++ middleware/verify-token.js
@@ -1,26 +1,3 @@
-// import JWT from 'jsonwebtoken'
+const authenticatedUser = (req,res,next) =>{
 
-// export const authenticateToken = (req, res, next) => {
-//   try {
-//     const authHeader = req.headers.authorization
-//     const token = authHeader && authHeader.split(' ')[1]
-
-//     if (!token) {
-//       return res.status(401).json({
-//         success: false,
-//         message: 'Access denied. No token provide',
-//       })
-//     }
-
-//     const decoded = JWT.verify(token, process.env.JWT_kEY)
-
-//     req.user = decoded
-//     next()
-//   } catch (error) {
-//     return res.status(401).json({
-//       success: false,
-//       message: 'Invalid or expired token',
-//       error: error.message,
-//     })
-//   }
-// }
+}
